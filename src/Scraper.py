85a56785c5e60b0db044249a10cd2e7530b07853
Kevin <marchinkev@gmail.com>
2022-03-19 20:14:23 +0000

    Viewport modification

--- src/Scraper.py
+++ src/Scraper.py
@@ -1,6 +1,10 @@
+import random
+
 from selenium import webdriver
-from fake_useragent import UserAgent
 from selenium.webdriver.chrome.options import Options
+
+from fake_useragent import UserAgent
+
 from webdriver_manager.chrome import ChromeDriverManager
 
 class IdSeleniumScraper():
@@ -14,6 +18,9 @@ class IdSeleniumScraper():
         options.add_argument(f'user-agent = {userAgent}')
 
         self.driver = webdriver.Chrome(executable_path = ChromeDriverManager().install(), chrome_options = options)
+
+        viewportList = [1920, 1080, 520, 1300, 650, 320]
+        self.driver.set_window_size(random.choice(viewportList), random.choice(viewportList))
 
     def connect(self, link):
         self.driver.get(link)
